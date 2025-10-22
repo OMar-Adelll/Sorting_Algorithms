4f38b69ce10883629620bb33a64e4d932a35b9c8
Omar Adel <OmarAdel.connect@gmail.com>
2025-10-23 00:24:14 +0300

    BubbleSort in Java

--- Java/Bubble_sort.java
+++ Java/Bubble_sort.java
@@ -1,11 +1,33 @@
 package Java;
+import java.util.Scanner;
 
 public class Bubble_sort {
-    void BubbleSort(int arr[]) {
-
+    static void BubbleSort(int nums[]) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] nums = new int[n];
 
+        for (int i = 0; i < n; i++)
+            nums[i] = in.nextInt();
+
+        BubbleSort(nums);
+
+        for (int num : nums)
+            System.out.print(num + " ");
+
+        in.close();
     }
 }
